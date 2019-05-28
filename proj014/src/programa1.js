be84b196dec544acdbf8c1826468c8cc0b599527
Helio Alves <heleralvess@gmail.com>
2019-05-28 18:37:11 -0300

    projeto 014: commit final

--- proj014/src/programa1.js
+++ proj014/src/programa1.js
@@ -9,11 +9,18 @@
 			Outros -> 35,90
 *******************************************************************************/
 
-
-
-
-
-
+function calcularFrete(uf) {
+  switch (uf) {
+    case "MG":
+      return 12.5;
+    case "SP":
+      return 23.9;
+    case "RJ":
+      return 18.2;
+    default:
+      return 35.9;
+  }
+}
 
 /*******************************************************************************
 	Tarefa 2: (USE SWITCH CASE)
@@ -23,11 +30,36 @@
 		"Mês inválido" caso seja usado algum número que não esteja entre 1 e 12.
 *******************************************************************************/
 
-
-
-
-
-
+function nomeMes(mes) {
+  switch (mes) {
+    case 1:
+      return "Janeiro";
+    case 2:
+      return "Fevereiro";
+    case 3:
+      return "março";
+    case 4:
+      return "Abril";
+    case 5:
+      return "Maio";
+    case 6:
+      return "Junho";
+    case 7:
+      return "Julho";
+    case 8:
+      return "agosto";
+    case 9:
+      return "Setembro";
+    case 10:
+      return "Outubro";
+    case 11:
+      return "Novembro";
+    case 12:
+      return "dezembro";
+    default:
+      return "Mês inválido";
+  }
+}
 
 /*******************************************************************************
 	Tarefa 3: (USE SWITCH CASE) 
@@ -37,3 +69,16 @@
 		Use essa referência para abranger todos os elementos:
 		https://pt.wikipedia.org/wiki/Tabela_peri%C3%B3dica#Estrutura_da_tabela_peri%C3%B3dica		
 *******************************************************************************/
+
+function tabelaPeriodica(elemento) {
+  switch (elemento) {
+    case "Zn":
+      return "zinco";
+    case "Pt":
+      return "platina";
+    case "Br":
+      return "bromo";
+    case "Sn":
+      return "estanho";
+  }
+}
